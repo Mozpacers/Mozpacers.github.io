1a46c35a096af8a5e0bdaeca986e058a51a60585
CuriousLearner <sanyam.khurana01@gmail.com>
2015-08-17 22:20:31 +0530

    Custom Validation: Different links added.

--- src/server/api/administration.py
+++ src/server/api/administration.py
@@ -25,15 +25,19 @@ class EView(ModelView):
         if field.data > form.event_end_date.data:
             raise ValidationError("Start Date should be earlier than End Date")
 
+    def different_links(form, field):
+        if (form.link.data == form.registration_form_link.data) or (form.link.data == form.event_image_link.data) or (form.event_image_link.data == form.registration_form_link.data):
+            raise ValidationError("All links should be different")
+
     form_args = dict(
         title=dict(label='Title', validators=[required()]),
-        link=dict(label='Link', validators=[required()]),
+        link=dict(label='Link', validators=[required(), different_links]),
         event_start_date=dict(label='Start Date', validators=[required(), future_events, start_end_date_validation]),
         event_end_date=dict(label='End Date', validators=[required(), future_events]),
         registration_form_link=dict(
-            label='Registration Form Link', validators=[required()]),
+            label='Registration Form Link', validators=[required(), different_links]),
         event_image_link=dict(
-            label='Event Featured Image Link', validators=[required()])
+            label='Event Featured Image Link', validators=[required(), different_links])
     )
 
     def is_accessible(self):
