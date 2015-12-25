7721b7edd629452a70f603050ec29ac3d96987ef
CuriousLearner <sanyam.khurana01@gmail.com>
2015-12-25 16:19:28 +0000

    Links fixed + smooth scroll added

--- src/client/js/script.js
+++ src/client/js/script.js
@@ -80,7 +80,24 @@ $(document).ready(function() {
     ctr = 0;
     validate_form();
   });
+  // For smooth scrolling -- Added by Sanyam (CuriousLearner)
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+  // Smooth scrolling end
 });
+
 /*
 nav-main-resp.js
 */
